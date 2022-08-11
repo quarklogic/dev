25a481b24495914752d0a3c45652b45f39d487e5
Carlos Ramirez <carlos@quarklogic.com>
2022-08-11 00:04:33 -0700

    Completed auto readindex method

--- readindex.py
+++ readindex.py
@@ -5,6 +5,53 @@ import sys
 import re
 import os.path
 
+def process_file(filename):
+
+    word_count      = 0
+    sentence_count  = 0
+    character_count = 0
+
+    ## Open up file for reading
+    contents = open(filename, 'r')
+
+    ## Loop through contents of file
+    for line in contents:
+        line = line.rstrip('\n')
+
+        print(f"LINE: {line}")
+
+        ## Split line into words
+        words = line.split()
+
+        for word in words:
+            word_count += 1
+
+            ## Get number of characters in word
+            word_length = len(word)
+
+            ## Increment character counter
+            character_count += word_length
+
+            print(f"word: {word} length: {word_length}")
+
+            ## Search for end of sentence markers: .!?
+            if re.search(r'[\.\?\!]$', word):
+                print(f"  found end of sentence")
+                sentence_count += 1
+
+        print("\n")
+
+    return [word_count, sentence_count, character_count]
+
+def autoread_method(stats):
+    word_count      = stats[0]
+    sentence_count  = stats[1]
+    character_count = stats[2]
+
+    readindex = 4.71 * (character_count / word_count) + \
+                0.5 * (word_count / sentence_count) - 21.43
+
+    return readindex
 
 
 if __name__ == '__main__':
@@ -12,7 +59,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--file', type=str, required=True)
-    parser.add_argument('--index', type=str, default="autoread")
+    parser.add_argument('--index', type=str, 
+        default="autoread")
 
     args = parser.parse_args()
 
@@ -21,8 +69,11 @@ if __name__ == '__main__':
         print(f"ERROR: File ({args.file}) does not exist!")
         exit()
 
+    stats = process_file(args.file)
 
+    readindex = autoread_method(stats)
 
+    print(f"read Index: {readindex}")
 
 
 
