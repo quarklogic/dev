09294acee15ec506ba8c5cc1ecc9fa8000a9c471
quarklogic <carlos@quarklogic.com>
2025-01-30 21:29:07 -0800

    Created windows2022_test.ps1 file

---
+++ scripting/windows2022_test.ps1
@@ -0,0 +1,14 @@
+# Script Name: 
+# Purpose: 
+
+Write-Host "Starting Script"
+
+# Print out the computer name
+#Invoke-Expression -Command 'hostname'
+
+
+#$password = Read-Host -AsSecureString
+#New-LocalUser -Name "TestUser1" -Password $password -FullName "Test User1"
+
+#auditpol.exe /set /category:* /success:enable | out-null
+#auditpol.exe /set /category:* /failure:enable | out-null
